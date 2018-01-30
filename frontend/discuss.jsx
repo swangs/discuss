2a77743c480ee9c28a905256ddaee8d3a43e2338
Steven Wang <sw1190@gmail.com>
2018-01-30 13:38:47 -0800

    add session api utils

--- frontend/discuss.jsx
+++ frontend/discuss.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+// TESTING
+import * as SessionApi from "./util/session_util";
+window.signUp = SessionApi.signUp;
+window.signIn = SessionApi.signIn;
+window.signOut = SessionApi.signOut;
+
+
+
+
+
+//TESTING
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>DISCUSS</h1>, root);
