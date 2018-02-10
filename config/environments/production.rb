0a178ea0523dd954ae6be12923083c2d9a99366a
Steven Wang <sw1190@gmail.com>
2018-02-09 16:04:49 -0800

    update productionrb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,13 +2,12 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV["s3_bucket"],
-      :access_key_id => ENV["s3_access_key_id"],
-      :secret_access_key => ENV["s3_secret_access_key"],
-      :s3_region => ENV["s3_region"],
-      :s3_host_name => "s3.#{ENV['s3_region']}.amazonaws.com"
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
     }
   }
 
